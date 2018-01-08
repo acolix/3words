90ea2a5521a28559c3244a16e0f43e6141f2e089
acolix <alicox@reed.edu>
2018-01-08 14:05:31 -0500

    fixed problems

--- wordscript.js
+++ wordscript.js
@@ -1,5 +1,6 @@
 var form =document.createElement('form');
 form.setAttribute('id','tripleform');
+//form.setAttribute('oninput','out.value=in1.value+in2.value+in3.value');
 //form.setAtribute('oninput')
 document.body.appendChild(form);
 var input1 = document.createElement('input');
@@ -8,7 +9,8 @@ var input3 = document.createElement('input');
 var button = document.createElement('input');
 var out = document.createElement('output');
 
-input1.setAttribute('type', 'text');
+//input1.setAttribute('type', 'text');
+input1.type='text'
 input1.setAttribute('id', 'in1');
 input2.setAttribute('type', 'text');
 input2.setAttribute('id', 'in2');
@@ -16,16 +18,18 @@ input3.setAttribute('type', 'text');
 input3.setAttribute('id', 'in3');
 button.setAttribute('type','submit');
 out.setAttribute('for','in1 in2 in3');
+out.name='out';
 
 form.appendChild(input1);
-form.appendChild(input1);
-form.appendChild(input1);
+form.appendChild(input2);
+form.appendChild(input3);
 form.appendChild(button);
 form.appendChild(out);
 
 function output (e){
-  e.prevenDefault()
-  out.setAttribute('value',String(input1.value+input2.value+input3.value))};
+  e.preventDefault();
+  out.value=String(input1.value+input2.value+input3.value);
+ };
 form.addEventListener('submit',output);
 
 
