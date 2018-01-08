60a4317393f2d80798a237298148d3eb90093e66
acolix <34978487+acolix@users.noreply.github.com>
2018-01-08 10:24:10 -0800

    Update wordscript.js

--- wordscript.js
+++ wordscript.js
@@ -8,12 +8,12 @@ var input3 = document.createElement('input');
 var button = document.createElement('input');
 var out = document.createElement('output');
 
-input1.setAttribute('type'= 'text');
-input1.setAttribute('id'= 'in1');
-input2.setAttribute('type'= 'text');
-input2.setAttribute('id'= 'in2');
-input3.setAttribute('type'= 'text');
-input3.setAttribute('id'= 'in3');
+input1.setAttribute('type', 'text');
+input1.setAttribute('id', 'in1');
+input2.setAttribute('type', 'text');
+input2.setAttribute('id', 'in2');
+input3.setAttribute('type', 'text');
+input3.setAttribute('id', 'in3');
 button.setAttribute('type','submit');
 out.setAttribute('for','in1 in2 in3');
 
