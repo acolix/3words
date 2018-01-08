366c7f1316d360f1d3cc4fd3e47c86a58fbc4e3c
acolix <34978487+acolix@users.noreply.github.com>
2018-01-08 10:20:35 -0800

    Update wordscript.js

--- wordscript.js
+++ wordscript.js
@@ -1,1 +1,31 @@
+var form =document.createElement('form');
+form.setAttribute('id','tripleform');
+form.setAtribute('oninput')
+document.body.appendChild(form);
+var input1 = document.createElement('input');
+var input2 = document.createElement('input');
+var input3 = document.createElement('input');
+var button = document.createElement('input');
+var out = document.createElement('output');
+
+input1.setAttribute('type'= 'text');
+input1.setAttribute('id'= 'in1');
+input2.setAttribute('type'= 'text');
+input2.setAttribute('id'= 'in2');
+input3.setAttribute('type'= 'text');
+input3.setAttribute('id'= 'in3');
+button.setAttribute('type','submit');
+out.setAttribute('for','in1 in2 in3');
+
+form.appendChild(input1);
+form.appendChild(input1);
+form.appendChild(input1);
+form.appendChild(submit);
+form.appendChild(out);
+
+function output (e){
+  e.prevenDefault()
+  out.setAttribute('value',String(input1.value+input2.value+input3.value))};
+form.addEventListener('submit',output);
+
 
